d228819d2a9d5b25c761e1686e68ce4d1c8cabe0
Demo User <demouser@MacBook-Pro.local>
2017-07-13 17:41:20 -0400

    blogasaurus

--- script.js
+++ script.js
@@ -6,12 +6,21 @@ function comment() {
 function handleKey(event) {
   if (event.keyCode == 13) {
     comment();
+    return false;
+  }
+}
+
+function animateHeading() {
+  for (var i = 0; i < 10; i++) {
+    $("h2").animate({fontSize: "40px"}, 1000);
+    $("h2").animate({fontSize: "25px"}, 1000);
   }
 }
 
 function setup() {
   $("#submit_button").click(comment);
   $("#comment").on('keypress', handleKey);
+  animateHeading();
 }
 
 $(document).ready(setup)
