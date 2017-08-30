09c005a7ea900b5d34b33ebb30159cd00fd1ce93
aljonpineda <aljonjbpineda@gmail.com>
2017-08-29 21:02:34 -0400

    Updated

--- static/script.js
+++ static/script.js
@@ -1,32 +1,44 @@
-function comment() {
+// Posts comments that users type in the textbox but
+// doesn't save them
+/*
+function postComment() {
   var username = $("#username").val();
   var comment = $("#comment").val();
 
-  $(".comments_area").append("<br>", username, " posted: ");
-  $(".comments_area").append(comment);
-}
-
-function handleKey(event) {
-  if (event.keyCode == 13) {
-    comment();
+  if (username == "" || comment == "") {
     return false;
+  }
+  else {
+    $(".comments_area").append("<br>", username, " posted: ");
+    $(".comments_area").append(comment, "<br>");
   }
 }
 
+// Doesn't work since pressing Enter refreshes the page
+/* function handleKey(event) {
+  if (event.keyCode == 13) {
+    postComment();
+    return false;
+  }
+} */
+
+// Animates the headings
 function animateHeading() {
+// Animates h2 headings
   $(".animated_heading h2").animate({fontSize: "35px"}, 800);
   $(".animated_heading h2").animate({fontSize: "24px"}, 800);
 
+// Animates h1 headings
   $(".animated_heading h1").animate({fontSize: "45px"}, 800);
   $(".animated_heading h1").animate({fontSize: "32px"}, 800);
 }
 
 function showParagraph() {
-  $("p").slideDown();
+  $(this).nextAll("p").slideDown();
 }
 
 function hideParagraph() {
-  $("p").slideUp();
+  $(this).nextAll("p").slideUp();
 }
 
 function showList() {
@@ -46,8 +58,8 @@ function mouseLeave() {
 }
 
 function setup() {
-  $("#submit_button").click(comment);
-  $("#comment").on("keypress", handleKey);
+//  $("#submit_button").click(postComment);
+//  $("#comment").on("keypress", handleKey);
 
   animateHeading();
 
